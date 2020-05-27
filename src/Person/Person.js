4b4382fa425d568eafb641f9131ec5093c812a56
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2020-05-26 23:18:21 -0400

    css modules and Media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,9 @@
 import React from "react";
-//import "./Person.css";
-import styled from "styled-components";
-
-const StyleDiv = styled.div`
-  width: 70%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    widows: 450px;
-  }
-`;
+import classes from "./Person.css";
 
 const person = (props) => {
-  /* const style = {
-    "@media(min-width:500px)": {
-      width: "450px",
-    },
-  };*/
-
   return (
-    <StyleDiv>
+    <div className={classes.person}>
       <h1>
         <p onClick={props.click}>
           I am {props.name} I have {props.age} years old
@@ -31,7 +11,7 @@ const person = (props) => {
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}></input>
       </h1>
-    </StyleDiv>
+    </div>
   );
 };
 
